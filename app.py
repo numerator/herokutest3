54df113f552a89e848dd678d965238371baa97f0
mwnewman <mwnewman@umich.edu>
2018-04-12 12:35:36 -0400

    homework 14 changes

--- app.py
+++ app.py
@@ -20,6 +20,16 @@ def postentry():
     model.add_entry(name, message)
     return redirect("/")
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html", entries=model.get_entries())
+
+@app.route("/delete", methods=["POST"])
+def delete():
+    id = request.form["id"]
+    model.delete_entry(id)
+    return redirect("/admin")
+
 if __name__=="__main__":
     model.init()
     app.run(debug=True)
--- model.py
+++ model.py
@@ -4,13 +4,16 @@ from datetime import datetime
 
 GUESTBOOK_ENTRIES_FILE = "entries.json"
 entries = []
+next_id = 0
 
 def init():
-    global entries
+    global entries, next_id
     try:
         f = open(GUESTBOOK_ENTRIES_FILE)
         entries = json.loads(f.read())
         f.close()
+        for e in entries:
+            next_id = max(next_id, e['id']) + 1
     except:
         entries = []
 
@@ -19,11 +22,12 @@ def get_entries():
     return entries
 
 def add_entry(name, text):
-    global entries, GUESTBOOK_ENTRIES_FILE
+    global entries, GUESTBOOK_ENTRIES_FILE, next_id
     now = datetime.now()
     time_string = str(now)
     # time_string = now.strftime("%b %d, %Y %-I:%M %p")
-    entry = {"author": name, "text": text, "timestamp": time_string}
+    entry = {"author": name, "text": text, "timestamp": time_string, "id": str(next_id)}
+    next_id += 1
     entries.insert(0, entry) ## add to front of list
     try:
         f = open(GUESTBOOK_ENTRIES_FILE, "w")
@@ -32,3 +36,12 @@ def add_entry(name, text):
         f.close()
     except:
         print("ERROR! Could not write entries to file.")
+
+def delete_entry(id):
+    global entries
+    to_delete = None
+    for e in entries:
+        if e["id"] == id:
+            to_delete = e
+    if to_delete is not None:
+        entries.remove(to_delete)
